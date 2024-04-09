ca1b91ccd9219ba84529ad019ec461237c29badd
lucas.maldonado <lucas.maldonado@bradescoseguros.com.br>
2024-04-09 14:45:46 -0300

    "Ajustado sro_vacuum.py"

--- notebooks/vs-opin-ingestao/batch/vacuum/sro_vacuum.py
+++ notebooks/vs-opin-ingestao/batch/vacuum/sro_vacuum.py
@@ -1,8 +1,10 @@
 # Databricks notebook source
 %sh pip uninstall -y opin-lib-ingestao-sro-dados
 
-# COMMAND ----------
-%run /vs-opin-ingestao/opin-lib-ingestao-sro-dados/batch/libs/libs
+import os
+if os.getenv("AMBIENTE"):
+    os.system("pip install /dbfs/FileStore/jars/commons/jproperties-2.1.1-py2.py3-none-any.whl")
+    os.system("pip install /dbfs/FileStore/jars/vs-opin-ingestao/opin-lib-cap-ingestao-sro-dados/opin_lib_cap_ingestao_sro_dados-1.0.0-py3-none-any.whl")
 
 # COMMAND ----------
 # DBTITLE 1,Imports
